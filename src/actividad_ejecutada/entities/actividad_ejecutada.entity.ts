a45ac91293a790d5a360f6cdc8326afae7a18751
Emerson <emersonbolanos42@gmail.com>
2026-04-16 18:19:13 -0500

    configurar entidad y DTOs de actividad_ejecutada

--- src/actividad_ejecutada/entities/actividad_ejecutada.entity.ts
+++ src/actividad_ejecutada/entities/actividad_ejecutada.entity.ts
@@ -1,1 +1,28 @@
-export class ActividadEjecutada {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('actividad_ejecutada')
+export class ActividadEjecutada {
+  @PrimaryGeneratedColumn()
+  id_actividad_ejecutada: number;
+
+  @Column()
+  id_cultivo_real: number;
+
+  @Column()
+  id_cultivo: number;
+
+  @Column({ type: 'date' })
+  fecha_ejecucion: Date;
+
+  @Column()
+  id_usuario: number;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  cantidad_usada: number;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  costo_aplicado: number;
+
+  @Column({ type: 'text', nullable: true })
+  observaciones: string;
+}
