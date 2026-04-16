cdfbf2e5b5e13b3212c70f54d9e3c30995dfec09
Emerson <emersonbolanos42@gmail.com>
2026-04-16 18:39:39 -0500

    agregar configuración TypeORM y conexion PostgresSQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ActividadEjecutadaModule } from './actividad_ejecutada/actividad_ejecutada.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT ?? '5432'),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    ActividadEjecutadaModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
