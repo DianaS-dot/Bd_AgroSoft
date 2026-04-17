cc4cc0805b1552a3128f469ca31c949d3b229dde
Emerson <emersonbolanos42@gmail.com>
2026-04-16 19:07:58 -0500

    registrar entidad en el modulo

--- src/actividad_ejecutada/actividad_ejecutada.module.ts
+++ src/actividad_ejecutada/actividad_ejecutada.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ActividadEjecutada } from './entities/actividad_ejecutada.entity';
 import { ActividadEjecutadaService } from './actividad_ejecutada.service';
 import { ActividadEjecutadaController } from './actividad_ejecutada.controller';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ActividadEjecutada])],
   controllers: [ActividadEjecutadaController],
   providers: [ActividadEjecutadaService],
 })
